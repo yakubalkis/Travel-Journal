60dfa38d186fd7248fd36e60402185fa81a05c8a
yakubalkis <yakupalkiis@gmail.com>
2022-07-31 18:01:17 +0300

    Hard-coded data uploaded.

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+import mountFuji from '../src/img/mount-fuji.png'
+import sydney from '../src/img/sydney.png'
+import norway from '../src/img/norway.png'
+
+
+export default [
+    {
+        id:1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7186087,15z/data=!4m13!1m7!3m6!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!2sMount+Fuji!3b1!8m2!3d35.3606255!4d138.7273634!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!5m1!1e4",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img: mountFuji
+    },
+    {
+        id:2,
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://www.google.com/maps/d/embed?mid=1NYBTZlF3StQEf65_sVbpMlskCoo&ie=UTF8&hl=en&msa=0&ll=-33.862148%2C151.21521899999996&spn=0.035636%2C0.042915&z=14&output=embed",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        img: sydney
+    },
+    {
+        id:3,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://www.google.com/maps/d/viewer?ie=UTF8&t=h&oe=UTF8&msa=0&mid=1r9iHu_NaY348ww7vQhCYC8wq-UA&ll=62.09023513589082%2C7.168744081213161&z=11",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: " The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        img: norway
+    }
+    
+]
+
